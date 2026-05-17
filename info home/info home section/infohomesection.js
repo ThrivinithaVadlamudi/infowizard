a6d2d497d98d6b41b71f7dc5528db59ff20abc3d
vadlamudi thrivinitha <thrivinithavadlamudi23@gmail.com>
2026-05-17 14:57:40 +0530

    initial commit

---
+++ info home/info home section/infohomesection.js
@@ -0,0 +1,11 @@
+function toggleMenu(){
+    document.getElementById("navLinks")
+    .classList.toggle("active");
+}
+function toggleMenu(){
+
+    let nav = document.querySelector(".nav-links");
+
+    nav.classList.toggle("active");
+
+}
